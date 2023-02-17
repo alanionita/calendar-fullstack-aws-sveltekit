be1e3ecf10a2ef22e16e7d8ae559eb0cda042057
alanionita <105461667+alanionita@users.noreply.github.com>
2023-02-17 03:49:12 +0000

    FEAT (layout) : adds basic layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<main class="layout-container">
+    <nav>
+        <a href="/">home</a>
+        <a href="/habits">habits</a>
+    </nav>
+    <slot />
+</main>
+
+<style>
+    .layout-container {
+        max-width: 1440px;
+        width: 100%;
+        margin: 0 auto;
+    }
+</style>
