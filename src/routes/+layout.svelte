f885f406f3d7612d7c2759af45d840e3685fb01b
alanionita <105461667+alanionita@users.noreply.github.com>
2023-02-17 04:36:51 +0000

    FEAT (ui) : created underlying UI grid, layout, and main components
    Also calculated the days in the current month
    
    And generated the day elements from that

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,30 @@
 <main class="layout-container">
-    <nav>
-        <a href="/">home</a>
-        <a href="/habits">habits</a>
-    </nav>
-    <slot />
+	<nav class="navbar">
+		<a href="/">home</a>
+		<a href="/habits">habits</a>
+	</nav>
+	<slot />
 </main>
 
 <style>
-    .layout-container {
-        max-width: 1440px;
-        width: 100%;
-        margin: 0 auto;
+	.layout-container {
+		max-width: 1440px;
+		width: 100%;
+        height: 100vh;
+		margin: 0 auto;
+        padding: 0 1rem;
+        background-color: blueviolet;
+        position: relative;
+	}
+	:global(body) {
+        --ui-grid-col-width: 103px;
+        --ui-grid-card-height: 9.6vw;
+		font-size: 16px;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+	}
+    .navbar {
+        height: 10%;
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	function returnAllDays() {
+		const currentDate = new Date(); // create a new Date object with the current date
+		const currentMonth = currentDate.getMonth(); // get the current month (0-11)
+
+		const firstDay = new Date(currentDate.getFullYear(), currentMonth, 1); // get the first day of the current month
+		const lastDay = new Date(currentDate.getFullYear(), currentMonth + 1, 0); // get the last day of the current month
+
+		const daysInMonth = []; // create an empty array to store the days in the current month
+
+		for (let day = firstDay.getDate(); day <= lastDay.getDate(); day++) {
+			// loop over each day in the current month
+			const date = new Date(currentDate.getFullYear(), currentMonth, day); // create a new Date object for the current day
+			daysInMonth.push(date); // add the new Date object to the array
+		}
+
+		console.log(daysInMonth); // log the array of days in the current month to the console
+        return daysInMonth
+	}
+
+	const days = returnAllDays();
+</script>
+
+<section class="home-grid">
+	<h1 class="home-month">Month</h1>
+	<ul class="home-days">
+		{#each days as day}
+			<li class="home-day">{`Day :: ${day}`}</li>
+		{/each}
+	</ul>
+</section>
+
+<style>
+	.home-grid {
+		display: grid;
+		height: 80%;
+        grid-template-columns: repeat(12, 1fr);
+		gap: 1em;
+        padding-top: 1rem;
+		background-color: palegoldenrod;
+	}
+	.home-month {
+        height: 100%;
+		writing-mode: vertical-lr;
+		grid-column-start: 1;
+		grid-column-end: span 1;
+	}
+	.home-days {
+		display: flex;
+		width: 100%;
+		flex-wrap: wrap;
+		grid-column-start: 2;
+		grid-column-end: span 11;
+		list-style: none;
+		padding: 0;
+        margin: 0;
+	}
+	.home-day {
+		background-color: aquamarine;
+		height: var(--ui-grid-card-height);
+
+		width: calc((100% / 7) - 16px);
+		margin-right: 1rem;
+	}
+	.home-day:nth-child(7n) {
+		background-color: red;
+		margin-right: 0rem;
+	}
+</style>
